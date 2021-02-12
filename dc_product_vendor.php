95f91de8580ddb83be1460506024f200a031d63e
DC-TGP-017 <rajsekhar@dualcube.com>
2021-02-12 12:48:56 +0530

    Plugin updated with version 3.5.18

--- dc_product_vendor.php
+++ dc_product_vendor.php
@@ -4,12 +4,12 @@
  * Plugin URI: https://wc-marketplace.com/
  * Description: A Free Extension That Transforms Your WooCommerce Site into a Marketplace.
  * Author: WC Marketplace
- * Version: 3.5.17
+ * Version: 3.5.18
  * Author URI: https://wc-marketplace.com/
  * Requires at least: 4.4
- * Tested up to: 5.6
+ * Tested up to: 5.6.1
  * WC requires at least: 3.0
- * WC tested up to: 4.9.2
+ * WC tested up to: 5.0.0
  *
  * Text Domain: dc-woocommerce-multi-vendor
  * Domain Path: /languages/
