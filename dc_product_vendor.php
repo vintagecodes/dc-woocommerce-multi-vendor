8be210609391f2e3f2066ef69b2b49702f996bd7
cool-raju <rajsekhar@dualcube.com>
2021-08-04 19:59:41 +0530

    Plugin updated with version 3.7.8

--- dc_product_vendor.php
+++ dc_product_vendor.php
@@ -4,12 +4,12 @@
  * Plugin URI: https://wc-marketplace.com/
  * Description: A Free Extension That Transforms Your WooCommerce Site into a Marketplace.
  * Author: WC Marketplace
- * Version: 3.7.7
+ * Version: 3.7.8
  * Author URI: https://wc-marketplace.com/
  * Requires at least: 4.4
  * Tested up to: 5.8
  * WC requires at least: 3.0
- * WC tested up to: 5.5.1
+ * WC tested up to: 5.5.2
  *
  * Text Domain: dc-woocommerce-multi-vendor
  * Domain Path: /languages/
