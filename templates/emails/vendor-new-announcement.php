7c5a7d50dfa35611700a5e303dc5169931f7baa1
cool-raju <rajsekhar@dualcube.com>
2021-11-05 13:54:07 +0530

    Fixed issue #971

--- templates/emails/vendor-new-announcement.php
+++ templates/emails/vendor-new-announcement.php
@@ -4,8 +4,8 @@
  *
  * Override this template by copying it to yourtheme/dc-product-vendor/emails/vendor-new-announcement.php
  *
- * @author 		WC Marketplace
- * @package 	dc-product-vendor/Templates
+ * @author      WC Marketplace
+ * @package     dc-product-vendor/Templates
  * @version   0.0.1
  */
 if (!defined('ABSPATH'))
@@ -18,7 +18,12 @@ $text_align = is_rtl() ? 'right' : 'left';
 <p><?php printf(esc_html__('%s', 'dc-woocommerce-multi-vendor'),  $post_title); ?></p>
 
 <?php $announcement_link = esc_url(wcmp_get_vendor_dashboard_endpoint_url(get_wcmp_vendor_settings('wcmp_vendor_announcements_endpoint', 'vendor', 'general', 'vendor-announcements'))); ?>
-<p><?php printf(esc_html__('This is to inform you that we recently updated the article %s. You can always check the changes from here  %s. We would request you to check the same and take the necessary action if required.', 'dc-woocommerce-multi-vendor'), $post_title, $announcement_link ); ?></p>
+
+<p><?php printf(esc_html__('This is to inform you that we recently updated the article %s :', 'dc-woocommerce-multi-vendor'), $post_title); ?></p>
+
+<p><?php echo esc_html($post_content); ?></p>
+
+<p><?php printf(esc_html__('You can always check the changes from here  %s. We would request you to check the same and take the necessary action if required.', 'dc-woocommerce-multi-vendor'), $announcement_link ); ?></p>
 
 <p><?php printf(esc_html__('%s continued use of the Store, will be subject to the updated terms.', 'dc-woocommerce-multi-vendor'), $single); ?></p>
 
